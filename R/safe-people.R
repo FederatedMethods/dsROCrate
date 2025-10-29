9b08c07f87b3afbf1c6e3d3f5a67149ec5b05af9
Roberto Villegas-Diaz <villegar@users.noreply.github.com>
2025-10-29 14:36:22 +0000

    Add new arguments to the safe_people.opal function to take a `user` object and a `user_id_suffix`. Add conditional to handle different inputs for `user`

--- R/safe-people.R
+++ R/safe-people.R
@@ -26,7 +26,7 @@ safe_people.character <- function(x, ..., rocrate = NULL) {
 }
 
 #' @export
-safe_people.opal <- function(x, ..., rocrate = NULL) {
+safe_people.opal <- function(x, ..., user = NULL, rocrate = NULL, user_id_suffix = "#person:") {
   # x is a valid opal connection object
   # TODO validate connection
 
@@ -34,20 +34,43 @@ safe_people.opal <- function(x, ..., rocrate = NULL) {
   safe_project_entity <- rocrate |>
     rocrateR::get_entity(type = "Project")
 
-  # extract user information
-  ## currently only username
-  ## create entity for user
-  user_entity <- rocrateR::entity(
-    x = paste0("#person:", digest::digest(x$username)),
-    type = "Person",
-    name = x$username,
-    # add all projects listed in the current RO-Crate
-    memberOf = safe_project_entity |>
-      sapply("[[", "@id") |>
-      lapply(function(id) {
-        list(`@id` = id)
-      })
-  )
+  # initialise empty user entity
+  user_entity <- NULL
+
+  # check if `user` was given
+  if (!is.null(user)) {
+    # check if user is a list, if so, then use all the elements as part of
+    # the user_entity
+    if ("list" %in% class(user)) {
+      user_entity <- rocrateR::entity(
+        x = c(getElement(user, "@id"), getElement(user, "id")),
+        type = "Person",
+        name = c(getElement(user, "name"), getElement(user, "username")),
+        affiliation = list(`@id` = c(getElement(user, "affiliation")))
+      )
+
+    } else {
+      user_entity <- rocrateR::entity(
+        x = paste0(user_id_suffix, digest::digest(user)),
+        type = "Person",
+        name = user
+      )
+    }
+  } else {
+    # extract user information from the connection object
+    user_entity <- rocrateR::entity(
+      x = paste0(user_id_suffix, digest::digest(x$username)),
+      type = "Person",
+      name = x$username
+    )
+  }
+
+  # add membership information, if Safe Project was found
+  if (length(safe_project_entity)) {
+    user_entity$memberOf <- safe_project_entity |>
+      lapply(\(x) list(`@id` = x[["@id"]]))
+  }
+
 
   # add user to the RO-Crate
   rocrate <- rocrate |>
