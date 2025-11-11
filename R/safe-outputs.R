efd00fc5701293f30ccc2aec5f7e4f18bf11af7f
Roberto Villegas-Diaz <villegar@users.noreply.github.com>
2025-11-11 09:49:12 +0000

    Update documentation and include `path` (if given) to write the logs

--- R/safe-outputs.R
+++ R/safe-outputs.R
@@ -2,20 +2,33 @@
 #'
 #' Safe outputs details for the RO-Crate.
 #'
-#' @param x PENDING
-#' @param ... Other optional arguments.
-#' @param rocrate RO-Crate object (see \link[rocrateR]{rocrate}).
-#' @param path description
-#' @param user description
-#' @param logs_to description
-#' @param logs_from description
-#' @param connection Connection object for the DataSHIELD server where the
-#'     values will be extracted from (e.g., OBiBa's Opal).
+#' All research outputs are checked to ensure individuals cannot be identified
+#' even in the public domain.
+#'
+#' This means the other four Safes no longer apply.
+#'
+#' Before outputs are released from the TRE, they are checked to make sure it
+#' is reasonably unlikely that any individuals can be identified on publication.
+#' Compliance with the Digital Economy Act also requires that the TRE applies
+#' methods and standards for output checking that are accredited by the UK
+#' Statistics Authority.
+#'
+#' @inheritParams safe_data
+#' @param ... Other optional arguments. See the full documentation,
+#'     [`?dsROCrate::safe_output`][safe_output()].
+#' @param path String to path pointing to the root of the RO-Crate. This will
+#'     be used to store log files. If not provided, logs will be stored within
+#'     the updated RO-Crate returned by this function.
+#' @param user String with the name of the user for which outputs will be
+#'     extracted. Optional, if no user is provided, then this will be extracted
+#'     from the `author` in the root (`./`) entity of the RO-Crate.
+#' @param logs_to Upper limit timestamp to filter outputs to be extracted
+#'     (default: `Sys.time()`, current system time).
+#' @param logs_from Lower limit timestamp to filter outputs to be extracted
+#'     (default: `Sys.time() - 24 * 60 ^ 2`, last 24 hours)
 #'
 #' @returns Updated RO-Crate object with Safe Outputs information.
 #' @export
-#'
-# @examples
 safe_output <- function(x, ...) {
   UseMethod("safe_output", x)
 }
@@ -134,7 +147,7 @@ safe_output.opal <- function(x, ..., rocrate = NULL, path = NULL, user = NULL, l
               call. = FALSE)
       log_entity$content <- list(userlogs)
     } else {
-       writeLines(userlogs, log_filename)
+       writeLines(userlogs, file.path(path, log_filename))
     }
   }
 
