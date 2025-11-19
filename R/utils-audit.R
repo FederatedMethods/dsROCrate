e0c948991852a87c869f1ba6a86325aba48c6065
Roberto Villegas-Diaz <villegar@users.noreply.github.com>
2025-11-19 10:08:51 +0000

    Update generic method for audit_safe_people.rocrate

--- R/utils-audit.R
+++ R/utils-audit.R
@@ -14,14 +14,59 @@ audit_safe_people <- function(x, ...) {
   UseMethod("audit_safe_people", x)
 }
 
+#' @rdname audit_safe_people
 #' @export
 audit_safe_people.default <- function(x, ...) {
-  stop("Unknown class, please try either a file path or",
-       " an object with `rocrate` class!")
+  stop(
+    "Unknown class, please try either a file path or",
+    " an object with `rocrate` class!"
+  )
 }
 
+#' @rdname audit_safe_people
+#' @export
+audit_safe_people.opal <- function(x, ..., project = NULL) {
+  stop(
+    "Unknown class, please try either a file path or",
+    " an object with `rocrate` class!"
+  )
+}
+
+#' @rdname audit_safe_people
 #' @export
 audit_safe_people.rocrate <- function(x, ...) {
-  stop("Unknown class, please try either a file path or",
-       " an object with `rocrate` class!")
+  # TODO: validate RO-Crate object
+
+  # extract author `@id`s from the root directory
+  author_ids <- rocrateR::get_entity(x, id = "./", type = "Dataset") |>
+    lapply(\(x) getElement(x, "author")) |>
+    sapply(\(x) getElement(x, "@id"))
+
+  # extract entities with type = 'Person'
+  person_entities <- rocrateR::get_entity(x, type = "Person")
+
+  # filter out person-entities in the `author_ids`
+  idx <- person_entities |>
+    sapply(\(x) getElement(x, "@id") %in% author_ids)
+  person_entities_v2 <- person_entities[idx]
+
+  # check if any entities were found
+  if (length(person_entities_v2) == 0) {
+    stop(
+      "No matching entities were found for the Author(s) in the root ",
+      "entity (./):\n",
+      paste0("  - ", author_ids, collapse = "\n"),
+      call. = FALSE
+    )
+  } else {
+    message(
+      length(person_entities_v2),
+      " 'Author' entit",
+      ifelse(length(person_entities_v2) == 1, "y was", "ies were"),
+      " found!"
+    )
+  }
+
+  # return invisibly
+  return(invisible(person_entities_v2))
 }
