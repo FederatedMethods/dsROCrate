0193e991ecebfafa7582f21a27e4b55f93041515
Roberto Villegas-Diaz <villegar@users.noreply.github.com>
2026-01-20 11:06:28 +0000

    Add new function refill_vec to reuse values from previous rows using group_by

--- R/utils-tibble.R
+++ R/utils-tibble.R
@@ -10,3 +10,19 @@ unfill_vec <- function(x, val = "") {
   same <- x == dplyr::lag(x)
   ifelse(!is.na(same) & same, val, x)
 }
+
+#' Fill vector
+#'
+#' @param x Input vector.
+#' @param val Default value.
+#'
+#' @returns Filled vector.
+#' @keywords internal
+refill_vec <- function(x, val = "") {
+  for (i in seq_along(x)) {
+    if (i > 1 && (is.na(x[i]) || x[i] == val)) {
+      x[i] <- x[i - 1]
+    }
+  }
+  x
+}
