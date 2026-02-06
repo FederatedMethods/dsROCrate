ebc72f3a23052015d5108f09cf8a05c2e4c3dfeb
Roberto Villegas-Diaz <villegar@users.noreply.github.com>
2026-02-06 17:16:16 +0000

    Update handling of RO-Crate bags as the input for rocrate_report

--- R/rocrate_report.R
+++ R/rocrate_report.R
@@ -38,7 +38,8 @@ rocrate_report.character <- function(
   # check if the given path points to an RO-Crate bag (zip file)
   if (grepl("zip$", tolower(x))) {
     # create temp directory to extract contents of RO-Crate
-    tempdir_name <- tempdir()
+    tempdir_name <- file.path(tempdir(), digest::digest(Sys.time()))
+    dir.create(tempdir_name, recursive = TRUE)
     on.exit(unlink(tempdir_name, force = TRUE, recursive = TRUE))
 
     # unbag RO-Crate
@@ -55,9 +56,28 @@ rocrate_report.character <- function(
   }
 
   # load RO-Crate
-  rocrate <- tryCatch(rocrateR::read_rocrate(roc_path), error = function(e) {
-    NULL
-  })
+  rocrate <- tryCatch(
+    {
+      # list JSON files, if roc_path points to a directory
+      if (dir.exists(roc_path)) {
+        roc_path_files <- list.files(
+          roc_path,
+          pattern = "[json|JSON]$",
+          recursive = TRUE,
+          full.names = TRUE
+        )
+        roc_path_files <- roc_path_files[1]
+        roc_path <- dirname(roc_path_files)
+      } else {
+        roc_path_files <- roc_path
+        roc_path <- dirname(roc_path)
+      }
+      rocrateR::read_rocrate(roc_path_files)
+    },
+    error = function(e) {
+      NULL
+    }
+  )
 
   # check if the RO-Crate was loaded correctly
   if (is.null(rocrate)) {
@@ -66,7 +86,7 @@ rocrate_report.character <- function(
 
   # check if any of the entities with `@type = 'File'` have empty `content`
   rocrate <- rocrate |>
-    load_content(roc_path = dirname(roc_path))
+    load_content(roc_path = roc_path)
 
   # call the next generic method
   rocrate |>
@@ -597,7 +617,10 @@ rocrate_report.rocrate <- function(
       dplyr::select(-id) |>
       dplyr::bind_cols(
         flatten_safe_project(safe_project_rocrate) |>
-          dplyr::select(-id)
+          dplyr::select(-id),
+        flatten_safe_data(safe_data_rocrate) |>
+          dplyr::select(-id) |>
+          dplyr::rename(table = name)
       )
   }
 
