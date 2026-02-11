6cfbd2570fc096edfebc5c43739787d7d2d60d89
Roberto Villegas-Diaz <villegar@users.noreply.github.com>
2026-02-11 16:33:55 +0000

    Rename test to match script name

--- tests/testthat/test-dsROCrate.R
+++ tests/testthat/test-dsROCrate.R
@@ -1,19 +1,14 @@
-setup({
-  skip_on_cran()
-  skip_if_offline()
-
-  source("opal-demo-server.R")
-})
-
 test_that("init() dispatches to init.opal() with real Opal connection", {
+  # setup
   crate <- rocrateR::rocrate_5s()
+  opal_con <- opal_demo_con()
 
   res <- init(
     opal_con,
     rocrate = crate,
-    project = PROJECT,
-    tables = TABLES,
-    user = PEOPLE
+    project = attr(opal_con, "PROJECT"),
+    tables = attr(opal_con, "TABLES"),
+    user = attr(opal_con, "PEOPLE")
   )
 
   expect_s3_class(res, "rocrate")
@@ -21,43 +16,48 @@ test_that("init() dispatches to init.opal() with real Opal connection", {
 })
 
 test_that("init.opal() attaches metadata using demo Opal server", {
+  # setup
   crate <- rocrateR::rocrate_5s()
+  opal_con <- opal_demo_con()
 
   res <- init.opal(
     opal_con,
     rocrate = crate,
     path = tempdir(),
-    project = PROJECT,
-    tables = TABLES,
-    user = PEOPLE
+    project = attr(opal_con, "PROJECT"),
+    tables = attr(opal_con, "TABLES"),
+    user = attr(opal_con, "PEOPLE")
   )
 
   expect_identical(attr(res, "connection"), opal_con)
-  expect_identical(attr(res, "project"), PROJECT)
-  expect_identical(attr(res, "tables"), TABLES)
-  expect_identical(attr(res, "user"), PEOPLE)
+  expect_identical(attr(res, "project"), attr(opal_con, "PROJECT"))
+  expect_identical(attr(res, "tables"), attr(opal_con, "TABLES"))
+  expect_identical(attr(res, "user"), attr(opal_con, "PEOPLE"))
   expect_true(dir.exists(attr(res, "path")))
 })
 
 test_that("init.rocrate() reuses stored attributes with demo Opal server", {
+  # setup
   crate <- rocrateR::rocrate_5s()
+  opal_con <- opal_demo_con()
 
   attr(crate, "connection") <- opal_con
-  attr(crate, "project") <- PROJECT
-  attr(crate, "tables") <- TABLES
-  attr(crate, "user") <- PEOPLE
+  attr(crate, "project") <- attr(opal_con, "PROJECT")
+  attr(crate, "tables") <- attr(opal_con, "TABLES")
+  attr(crate, "user") <- attr(opal_con, "PEOPLE")
   attr(crate, "path") <- tempdir()
 
   res <- init(crate)
 
   expect_identical(attr(res, "connection"), opal_con)
-  expect_identical(attr(res, "project"), PROJECT)
-  expect_identical(attr(res, "tables"), TABLES)
-  expect_identical(attr(res, "user"), PEOPLE)
+  expect_identical(attr(res, "project"), attr(opal_con, "PROJECT"))
+  expect_identical(attr(res, "tables"), attr(opal_con, "TABLES"))
+  expect_identical(attr(res, "user"), attr(opal_con, "PEOPLE"))
   expect_identical(attr(res, "path"), attr(crate, "path"))
 })
 
 test_that("init.opal() errors on invalid connection object", {
+  # setup
   crate <- rocrateR::rocrate_5s()
 
   expect_error(
