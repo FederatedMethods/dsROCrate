671824792213299173bffa32caff55c858fa8205
Roberto Villegas-Diaz <villegar@users.noreply.github.com>
2026-07-17 13:14:14 +0100

    Add new S3 generic for as.data.frame.safe_call objects and update the constructor for safe_call, so it captures additional arguments captured in the logs (e.g., session and user)

--- R/safe-call.R
+++ R/safe-call.R
@@ -1,3 +1,17 @@
+#' @export
+as.data.frame.safe_call <- function(x, ...) {
+  data.frame(
+    timestamp = format(x$created_at, '%Y-%m-%dT%H:%M:%S'),
+    user = x$user,
+    r_cmd = x$original,
+    fx = paste0(x$package, x$namespace, x$fx),
+    args = x$args,
+    session = x$session,
+    profile = x$profile,
+    stringsAsFactors = FALSE
+  )
+}
+
 #' Construct a safe_call
 #'
 #' @keywords internal
@@ -7,7 +21,7 @@ new_safe_call <- function(
   namespace,
   fx,
   args,
-  expr = NULL
+  ...
 ) {
   stopifnot(is.character(original))
   stopifnot(is.character(fx))
@@ -20,25 +34,28 @@ new_safe_call <- function(
       namespace = namespace,
       fx = fx,
       args = args,
-      expr = expr
+      created_at = getElement(list(...), "@timestamp"),
+      user = getElement(list(...), "username"),
+      session = getElement(list(...), "ds_id"),
+      profile = getElement(list(...), "ds_profile")
     ),
     class = "safe_call"
   )
 }
 
-safe_call <- function(call) {
+safe_call <- function(call, ...) {
   UseMethod("safe_call")
 }
 
 #' @export
-safe_call.character <- function(call) {
+safe_call.character <- function(call, ...) {
   expr <- str2lang(call)
-  safe_call(expr)
+  safe_call(expr, ...)
 }
 
 #' @export
-safe_call.call <- function(call) {
+safe_call.call <- function(call, ...) {
   parsed <- parse_call(call)
 
-  do.call(new_safe_call, parsed)
+  do.call(new_safe_call, c(parsed, list(...)))
 }
