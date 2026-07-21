e438eda662e4469dc91d1f5054a49cae752708ae
Roberto Villegas-Diaz <villegar@users.noreply.github.com>
2026-07-21 17:06:41 +0100

    Redefine as.data.frame.safe_symbol generic to as_tibble.safe_symbol

--- R/safe-symbol.R
+++ R/safe-symbol.R
@@ -1,19 +1,19 @@
 #' @export
-as.data.frame.safe_symbol <- function(x, ...) {
-  data.frame(
+as_tibble.safe_symbol <- function(x, ...) {
+  tibble::tibble(
     id = x$id,
     symbol = x$symbol,
     kind = x$kind,
     asset = x$asset,
     expr = x$expr,
-    # parents = x$parents,
+    depends_on = list(x$depends_on),
     created_by = x$created_by,
     created_at = x$created_at,
     user = x$user,
     session = x$session,
-    action = x$action,
+    action = x$action #,
     # metadata = x$metadata,
-    stringsAsFactors = FALSE
+    # stringsAsFactors = FALSE
   )
 }
 
@@ -22,7 +22,7 @@ new_safe_symbol <- function(
   kind = "unknown",
   asset = NA_character_,
   expr = NA_character_,
-  parents = list(),
+  depends_on = list(),
   created_by = NA_character_,
   created_at = Sys.time(),
   user = NA_character_,
@@ -41,7 +41,7 @@ new_safe_symbol <- function(
       kind = kind,
       asset = asset,
       expr = expr,
-      parents = parents,
+      depends_on = depends_on,
       created_by = created_by,
       created_at = created_at,
       user = user,
@@ -53,8 +53,25 @@ new_safe_symbol <- function(
   )
 }
 
+new_safe_symbol_reference <- function(symbol, column = NULL) {
+  stopifnot(is.character(symbol))
+  stopifnot(length(symbol) == 1)
+
+  structure(
+    list(
+      symbol = symbol,
+      column = column
+    ),
+    class = "safe_symbol_reference"
+  )
+}
+
 safe_symbol <- function(symbol, ...) {
   new_safe_symbol(symbol = symbol, ...)
+}
+
+safe_symbol_reference <- function(symbol, ...) {
+  new_safe_symbol_reference(symbol = symbol, ...)
 }
 
 symbol <- function(x, ...) {
@@ -94,15 +111,14 @@ symbol_metadata.safe_symbol <- function(x, ...) {
   x$metadata
 }
 
-symbol_parents <- function(x, ...) {
-  UseMethod("symbol_parents")
+symbol_depends_on <- function(x, ...) {
+  UseMethod("symbol_depends_on")
 }
 
 #' @export
-symbol_parents.safe_symbol <- function(x, ...) {
-  x$parents
+symbol_depends_on.safe_symbol <- function(x, ...) {
+  x$depends_on
 }
-
 
 symbol_session <- function(x, ...) {
   UseMethod("symbol_session")
