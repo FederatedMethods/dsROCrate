af152e9d3d9057baba3f05544189b5a70fe7b78b
Roberto Villegas-Diaz <villegar@users.noreply.github.com>
2026-07-22 17:11:50 +0100

    Update report to use new 'asset' column, instead of 'table'

--- R/report.R
+++ R/report.R
@@ -504,11 +504,11 @@ report.rocrate <- function(
   safe_output_tbl_v2 <- tibble::tibble()
 
   if (!is.null(safe_output_tbl) && nrow(safe_output_tbl) > 0) {
-    # split `table` into `project` and `table`
+    # split `asset` into `project` and `asset`
     safe_output_tbl_v2 <- safe_output_tbl |>
       dplyr::mutate(
-        project = gsub("(?=\\.).*$", "", table, perl = TRUE),
-        asset = gsub("^.*(?<=\\.)", "", table, perl = TRUE)
+        project = gsub("(?=\\.).*$", "", asset, perl = TRUE),
+        asset = gsub("^.*(?<=\\.)", "", asset, perl = TRUE)
       ) |>
       dplyr::distinct(project, asset, user, fx, timestamp)
 
@@ -544,7 +544,7 @@ report.rocrate <- function(
   report_contents <- c(
     .markdown_report_header(title, overview_tbl, overview_lst$diag_path),
     tidy_overview_tbl |>
-      # # tidy up duplicated values in `project` and `table`
+      # # tidy up duplicated values in `project` and `asset`
       # dplyr::mutate(
       #   Project = unfill_vec(Project),
       #   Data = unfill_vec(Data)
