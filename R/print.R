9d3ed3518add17ca165ebc8915d2d2b0d313fd06
Roberto Villegas-Diaz <villegar@users.noreply.github.com>
2026-07-30 11:13:19 +0100

    Update print.safe_symbol S3 generic

--- R/print.R
+++ R/print.R
@@ -80,15 +80,15 @@ print.safe_symbol <- function(x, ...) {
   msg <- c(msg, paste("Symbol :", x$symbol))
   msg <- c(msg, paste("Kind   :", x$kind))
 
-  if (!is.null(x$asset)) {
+  if (!is.null(x$asset) && !is.na(x$asset)) {
     msg <- c(msg, paste("Asset  :", x$asset))
   }
 
-  if (!is.null(x$expr)) {
+  if (!is.null(x$expr) && !is.na(x$expr)) {
     msg <- c(msg, paste("Expression :", x$expr))
   }
 
-  if (!is.null(x$session)) {
+  if (!is.null(x$session) && !is.na(x$session)) {
     msg <- c(msg, paste("Session :", x$session))
   }
 
